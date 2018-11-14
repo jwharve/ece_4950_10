32885f9ebdff835c370184742fef17a1575a3852
Josh Harvey <jwharve@g.clemson.edu>
2018-11-14 03:25:22 -0500

    renamed model and added file for control code

---
+++ control.m
@@ -0,0 +1,11 @@
+clearvars -except tg; close all; clc;
+
+if ~exist('tg','var') % check to see if the target is already built
+        rtwbuild('motor_param_test');
+end
+tg.load('motor_param_test');
+tg.start;
+
+tg.setparam(tg.getparamid('Switch','Value'),sim)
+
+tg.stop;
